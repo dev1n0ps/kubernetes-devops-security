e9b56cf294dd5cd953e8e3aa10feef835fea0ebb
Dev <dev.bingeops@posteo.de>
2024-12-13 00:20:00 +0100

    Added open-api specification to springboot app

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;  // JUnit 5 import
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,4 +41,7 @@ public class NumericApplicationTests {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
 }
+
+
