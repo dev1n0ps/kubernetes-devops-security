9d055897a684fa360915afb76a436d84e729c293
Dev <dev.bingeops@posteo.de>
2024-12-13 15:42:31 +0100

    downgraded springboot version

--- src/main/java/com/devsecops/NumericApplication.java
+++ src/main/java/com/devsecops/NumericApplication.java
@@ -2,9 +2,21 @@ package com.devsecops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class NumericApplication {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(security -> security
+						.requestMatchers("/increment/**").permitAll()
+				.anyRequest().authenticated());
+
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(NumericApplication.class, args);
